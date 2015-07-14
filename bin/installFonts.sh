dffccb0c9cead2164dbbd6b5cc36691e0e91814d
cmaeder <c.maeder@jacobs-university.de>
2015-07-14 13:40:14 +0200

    script for font installation under debian/ubuntu

---
+++ bin/installFonts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# execute as root (via sudo)
+fontdir="$(dirname $0)/../fonts"
+mkdir -p /usr/share/fonts/opentype/Fandol
+mkdir -p /usr/share/fonts/truetype/cwTeX
+cp "${fontdir}/FandolFang-Regular.otf" /usr/share/fonts/opentype/Fandol/
+cp "${fontdir}/cwTeXQKai-Medium.ttf" /usr/share/fonts/truetype/cwTeX/
+exec fc-cache
