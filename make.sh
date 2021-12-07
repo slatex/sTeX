569859deff9b0c89587e2140d28cec34f7956832
Dennis Müller <d.mueller@kwarc.info>
2021-12-07 20:45:54 +0100

    update

--- make.sh
+++ make.sh
@@ -4,8 +4,21 @@ pdflatex stex.ins
 # pdflatex basics.dtx
 # pdflatex mathhub.dtx
 # pdflatex mathhub.dtx
+cd ../extensions
+pdflatex extensions.ins
+pdflatex omdoc.dtx
+biber omdoc
+pdflatex omdoc.dtx
+pdflatex mikoslides.dtx
+biber mikoslides
+pdflatex mikoslides.dtx
+pdflatex tikzinput.dtx
+biber mikoslides
+pdflatex tikzinput.dtx
 cd ../../doc
 pdflatex stex.tex
-pdflatex manual.tex
-pdflatex manual.tex
+biber stex
 pdflatex stex.tex
+pdflatex manual.tex
+biber manual
+pdflatex manual.tex
